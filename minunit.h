d6b1f3f40473e49281ec4687aac75625ff1fcd45
Rolf Kalbermatter <15158041+RolfKal@users.noreply.github.com>
2024-11-18 15:50:04 +0100

    Add timing information to each unit test step

--- minunit.h
+++ minunit.h
@@ -5,16 +5,21 @@
 
 #ifndef _MINUNIT_HEADER
 #define _MINUNIT_HEADER
+#include <time.h>
 
 #define mu_test(_s, _c) \
 do { \
+    double executed; \
+    clock_t start = clock(); \
 	printf("#%03d %s \n", ++mu_tests, _s); \
 	if (_c) { \
-		printf("PASSED\n"); \
+		printf("PASSED"); \
 	} else { \
-		printf("FAILED\n"); \
+		printf("FAILED"); \
 		mu_fails++; \
 	} \
+    executed = (double)(clock() - start) / CLOCKS_PER_SEC; \
+	printf(", Duration: %.3f secs\n", executed); \
 } while (0)
 
 extern int mu_tests, mu_fails;
